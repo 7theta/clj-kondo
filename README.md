f2003a0cb15589e38d589edc3a84af3ae84ad92d
Achint Sandhu <sandhu@7theta.com>
2022-07-21 15:18:00 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# com.7theta/clj-kondo
+
+[![Current Version](https://img.shields.io/clojars/v/com.7theta/clj-kondo.svg)](https://clojars.org/com.7theta/clj-kondo)
+[![GitHub license](https://img.shields.io/github/license/7theta/clj-kondo.svg)](LICENSE)
+
+`clj-kondo` configs for linting 7theta projects.
+
+## Copyright and License
+
+Copyright © 2015 7theta
+
+Distributed under the MIT License.
+
---
+++ project.clj
@@ -0,0 +1,16 @@
+;;   Copyright (c) 7theta. All rights reserved.
+;;   The use and distribution terms for this software are covered by the
+;;   MIT License (https://opensource.org/licenses/MIT) which can also be
+;;   found in the LICENSE file at the root of this distribution.
+;;
+;;   By using this software in any fashion, you are agreeing to be bound by
+;;   the terms of this license.
+;;   You must not remove this notice, or any others, from this software.
+
+(defproject com.7theta/clj-kondo "0.1.0"
+  :description "clj-kondo configs for 7theta projects"
+  :url "https://github.com/7theta/clj-kondo"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :scm {:name "git"
+        :url "https://github.com/7theta/clj-kondo"})
